f9c3082b675b92676c4ceece828899586355b7f6
ThazinWai63 <e1332403@u.nus.edu>
2024-02-08 00:52:39 +0800

    main

---
+++ app/components/common/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      This is Footer Testing.
+    </div>
+  )
+}
+
+export default Footer
---
+++ app/components/common/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      This is Nav Bar.
+    </div>
+  )
+}
+
+export default NavBar
