bc6eda30b72dbda08f54b0d2acbb66119b51dcda
KhinYadanarNg <e0015793@u.nus.edu>
2024-02-13 14:24:43 +0800

    show two columns in campaign card

--- app/components/Campaign.tsx
+++ app/components/Campaign.tsx
@@ -8,7 +8,7 @@ const Campaign = ({ campaign }: CampaignProps) => {
         <div className='border px-2 py-2'>
             <h3 className='pb-2'>{description}</h3>
             <ProgressBar></ProgressBar>
-            <div className='border w-58 h-40 pt-2 px-2 mt-3 grid 2xl:grid-cols-4 md:grid-cols-2 grid-cols-1'>
+            <div className='border w-58 h-40 pt-2 px-2 mt-3 grid 2xl:grid-cols-4 md:grid-cols-2 grid-cols-2'>
                 <div className='border h-32 flex justify-center items-center bg-orange-500'>{store}</div>
                 <text className='px-2 w-full'>${discount} off<br />
                     Min Spend: ${minimumSpend}</text>
