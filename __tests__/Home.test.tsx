5cd95b389c95469fcda9b40959e638f19a5d6d9d
KhinYadanarNg <e0015793@u.nus.edu>
2024-02-17 11:38:24 +0800

    add reset password ui and test case

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,10 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/app/page'; // Import the component you want to test
 
-test('Home component should have Login text', () => {
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {
+      prefetch: () => null
+    };
+  }
+}));
+
+test('Home component should have text', () => {
   render(<Home />); // Arrange: Render the component
 
-  const myElem = screen.getByText('Login'); // Act: Get the element with text 'Login'
+  const myElem = screen.getByText('This is campaign list'); // Act: Get the element with text 'Login'
 
   expect(myElem).toBeInTheDocument(); // Assert: Check if the element is in the document
 });
