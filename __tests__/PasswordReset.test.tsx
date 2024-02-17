6c2c8aff8516629a7155320afefe134c97ce7f62
KhinYadanarNg <e0015793@u.nus.edu>
2024-02-17 11:40:15 +0800

    format code

--- __tests__/PasswordReset.test.tsx
+++ __tests__/PasswordReset.test.tsx
@@ -3,34 +3,34 @@ import PasswordResetUi from '@/app/components/passwordReset/PasswordResetUi';
 import { render, screen } from '@testing-library/react';
 
 jest.mock("next/navigation", () => ({
-  useRouter() {
-    return {
-      prefetch: () => null
-    };
-  }
+    useRouter() {
+        return {
+            prefetch: () => null
+        };
+    }
 }));
 
 test('Reset password component should have header text', () => {
-    render(<PasswordResetUi />); 
+    render(<PasswordResetUi />);
     const header = screen.getByRole("heading");
     const headerText = 'Welcome to IV Voucher Management';
     expect(header).toHaveTextContent(headerText);
-  });
+});
 
-  test('Reset password component should have button', () => {
-    render(<PasswordResetUi />); 
-    const buttonElement = screen.getByRole('button'); 
+test('Reset password component should have button', () => {
+    render(<PasswordResetUi />);
+    const buttonElement = screen.getByRole('button');
     expect(buttonElement).toHaveClass('authentication__btn');
-  });
-  
-  test('Reset passwordt component should have input fields', () => {
-    render(<PasswordResetUi />); 
-    const inputElementEmail = screen.getByTestId('input-field-email'); 
-    expect(inputElementEmail).toHaveClass('logintext__input');
-    const inputElementPassword = screen.getByTestId('input-field-password'); 
-    expect(inputElementPassword).toHaveClass('logintext__input');
-  });
-  
+});
 
-  
+test('Reset passwordt component should have input fields', () => {
+    render(<PasswordResetUi />);
+    const inputElementEmail = screen.getByTestId('input-field-email');
+    expect(inputElementEmail).toHaveClass('logintext__input');
+    const inputElementPassword = screen.getByTestId('input-field-password');
+    expect(inputElementPassword).toHaveClass('logintext__input');
+});
+
+
+
 
--- app/components/passwordReset/PasswordResetUi.tsx
+++ app/components/passwordReset/PasswordResetUi.tsx
@@ -5,46 +5,46 @@ import { hasWhiteSpace, isValidateEmail } from '@/utils';
 import { useRouter } from 'next/navigation';
 
 const PasswordResetUi = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
 
 
-    const resetPassword = async () => {
-        const isValidEmail = isValidateEmail(email);
-        const hasPasswordWhiteSpace = hasWhiteSpace(password);
-        if (!isValidEmail) {
-            alert("Please provide valid email");
-            return;
-        }
-        if (password.length > 0 && !hasPasswordWhiteSpace) {
-            router.push('/components/login');
-        } else {
-            alert("please provide valid password")
-        }
+  const resetPassword = async () => {
+    const isValidEmail = isValidateEmail(email);
+    const hasPasswordWhiteSpace = hasWhiteSpace(password);
+    if (!isValidEmail) {
+      alert("Please provide valid email");
+      return;
     }
+    if (password.length > 0 && !hasPasswordWhiteSpace) {
+      router.push('/components/login');
+    } else {
+      alert("please provide valid password")
+    }
+  }
 
-    return (
-        <main>
-            <div className="mt-20">
-                {/* <h1 className="text-4x1 font-extrabold text-center">Welcome to IV Voucher Manage</h1> */}
-                <Heading title={'Welcome to IV Voucher Management'} center={true} />
-            </div>
-            <div className='wrapper mt-5'>
-                <form action="/" method="post" >
-                    <div className='registration__input'>
-                        Email
-                    </div>
-                    <input type="text" id="email" className="logintext__input" data-testid ='input-field-email' onChange={(e) => setEmail(e.target.value)} />
-                    <div className='registration__input'>
-                        Password
-                    </div>
-                    <input type="password" id="password" className="logintext__input" data-testid ='input-field-password' onChange={(e) => setPassword(e.target.value)} />
-                    <button type="button" className="authentication__btn mt-10" onClick={resetPassword}>Reset Password</button>
-                </form>
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div className="mt-20">
+        {/* <h1 className="text-4x1 font-extrabold text-center">Welcome to IV Voucher Manage</h1> */}
+        <Heading title={'Welcome to IV Voucher Management'} center={true} />
+      </div>
+      <div className='wrapper mt-5'>
+        <form action="/" method="post" >
+          <div className='registration__input'>
+            Email
+          </div>
+          <input type="text" id="email" className="logintext__input" data-testid='input-field-email' onChange={(e) => setEmail(e.target.value)} />
+          <div className='registration__input'>
+            Password
+          </div>
+          <input type="password" id="password" className="logintext__input" data-testid='input-field-password' onChange={(e) => setPassword(e.target.value)} />
+          <button type="button" className="authentication__btn mt-10" onClick={resetPassword}>Reset Password</button>
+        </form>
+      </div>
+    </main>
+  )
 }
 
 export default PasswordResetUi
