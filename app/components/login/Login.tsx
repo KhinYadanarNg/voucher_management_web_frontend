b185332f3a51251917896ebc3aef92b3a60136d8
KhinYadanarNg <e0015793@u.nus.edu>
2024-02-18 23:30:40 +0800

    call login api and check response

--- app/components/login/Login.tsx
+++ app/components/login/Login.tsx
@@ -4,13 +4,14 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Heading from "../common/Heading";
+import { loginUser } from "@/app/service/authentication";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
 
-  const logIn = () => {
+  const logIn = async () => {
     const isValidEmail = isValidateEmail(email);
     const hasPasswordWhiteSpace = hasWhiteSpace(password);
     if (!isValidEmail) {
@@ -18,7 +19,17 @@ const Login = () => {
       return;
     }
     if (password.length > 0 && !hasPasswordWhiteSpace) {
-      router.push("/");
+      try {
+        const response = await loginUser(email, password);
+        const { message, result } = response;
+        alert(message);
+        if (result.length > 0) {
+          router.push("/");
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+      }
     } else {
       alert("please provide valid password");
     }
--- app/service/authentication.ts
+++ app/service/authentication.ts
@@ -52,37 +52,27 @@ export async function forgotPassword(email: string, password: string) {
     return result;
 }
 
-export const fetchCampaigns = async () => {
-    const result =
-    [
-            {
-                "id": 1,
-                "code": "VOUCHER10",
-                "discount": 10,
-                "description": "Get 10% off on all products",
-                "store": "NTUC",
-                "minimumSpend": 100,
-                "policy": "Term and condition to use the voucher, enjoy with our voucher by registering as a member. "
-            },
-            {
-                "id": 2,
-                "code": "VOUCHER20",
-                "discount": 20,
-                "description": "Get 20% off on your next purchase",
-                "store": "Cold Storage",
-                "minimumSpend": 100,
-                "policy": "Term and condition to use the voucher, enjoy with our voucher by registering as a member. "
-            },
-            {
-                "id": 3,
-                "code": "FREESHIP",
-                "discount": 30,
-                "description": "Free shipping on orders above $50",
-                "store": "Zara",
-                "minimumSpend": 100,
-                "policy": "Term and condition to use the voucher, enjoy with our voucher by registering as a member. "
-            }
-        ]
-    const response = JSON.parse(result.toString());
-    return response
+export async function loginUser(email: string, password: string) {
+
+    let body = {
+        email,
+        password
+    }
+
+    console.log(JSON.stringify(body));
+    const headers = {
+        'Content-Type': 'application/json'
+    };
+
+    const response = await fetch(
+        `http://localhost:8080/api/user/login`,
+        {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body)
+
+        });
+
+    const result = await response.json();
+    return result;
 }
