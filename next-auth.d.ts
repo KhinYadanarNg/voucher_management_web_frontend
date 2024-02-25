7c26682930331b21327b984e179ac1f00fd16eb7
KhinYadanarNg <e0015793@u.nus.edu>
2024-02-25 16:11:26 +0800

    define user session type

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,5 +1,11 @@
 import NextAuth from "next-auth";
 
+declare module "next-auth" {
+  interface Session {
+    user: User
+  }
+}
+
 declare module "next-auth/jwt" {
   interface JWT {
     user?: DefaultUser;
