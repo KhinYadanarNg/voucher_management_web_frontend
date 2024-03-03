78b52ac958b4820ac13ccdbd1b9aff46b2157298
KhinYadanarNg <e0015793@u.nus.edu>
2024-03-03 22:43:32 +0800

    change server component

--- app/components/merchant/stores/detail/page.tsx
+++ app/components/merchant/stores/detail/page.tsx
@@ -1,26 +1,19 @@
-'use client';
-import { useEffect, useState } from "react";
 import StoreDetail from "./StoreDetail";
 import { StoreDetailProps } from "@/type/store";
 import NullData from "@/app/components/common/NullData";
-import { useSession } from "next-auth/react";
+import { getCurrentUser } from "@/app/auth/getCurrentUser";
 
-const DetailStore = () => {
-    const [storeDetail, setStoreDetail] = useState<StoreDetailProps>();
-    useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
-        const storeDetail = params.get('store');
-
-        const decodedStoreData = storeDetail ? JSON.parse(storeDetail.toString()) : null;
-        setStoreDetail(decodedStoreData);
-    }, []);
-
-    const { data: session } = useSession();
-    const currentUser = session?.user;
+const DetailStore = async ({ searchParams }: {
+    searchParams: {
+        store: StoreDetailProps
+    }
+}) => {
+    const currentUser = await getCurrentUser();
+    const decodedStoreData = searchParams.store ? JSON.parse(searchParams.store.toString()) : null;
 
     {
         return (
-            currentUser ? (<div>{storeDetail && <StoreDetail store={storeDetail} />}</div>) : (
+            currentUser ? (<div>{decodedStoreData && <StoreDetail store={decodedStoreData} />}</div>) : (
                 <NullData title="Oops Access denied" />
             )
         );
