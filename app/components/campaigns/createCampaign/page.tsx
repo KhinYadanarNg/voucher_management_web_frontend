8a30dcabd9b3c78e08f105b1212261e6e4e639aa
KhinYadanarNg <e0015793@u.nus.edu>
2024-03-07 00:24:01 +0800

    add create campaign form

--- app/components/campaigns/createCampaign/page.tsx
+++ app/components/campaigns/createCampaign/page.tsx
@@ -1,9 +1,37 @@
+import { fetchStoreListByMerchant } from '@/app/service/store';
 import React from 'react'
+import NullData from '../../common/NullData';
+import CreateCampaignForm from './CreateCampaignForm';
+import { getCurrentUser } from '@/app/auth/getCurrentUser';
 
-const CreateCampaign = () => {
-  return (
-    <div>page</div>
-  )
+const getStoreList = async () => {
+  try {
+    const storeList = await fetchStoreListByMerchant();
+    return storeList;
+  } catch (error) {
+    console.log(error);
+  } finally {
+
+  }
 }
+export default async function CreateCampaign() {
+  const stores = await getStoreList();
+  const currentUser = await getCurrentUser();
 
-export default CreateCampaign
+  if (!currentUser || currentUser.role !== "MERCHANT") {
+    return <NullData title="Oops! Access denied" />;
+  }
+  {
+    return (
+      stores ? (
+        <section className='py-5'>
+          <div className='container'>
+            <CreateCampaignForm stores={stores.result}></CreateCampaignForm>
+          </div>
+        </section>
+      ) : (
+        <NullData title="Fetch data failed" />
+      ))
+  }
+
+}
