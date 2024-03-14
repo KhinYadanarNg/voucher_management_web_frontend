d52cf86dee76c5eb257f36627bed2eed4216d021
KhinYadanarNg <e0015793@u.nus.edu>
2024-03-14 18:39:01 +0800

    add test case for campaign list

---
+++ __tests__/CampaignList.test.tsx
@@ -0,0 +1,22 @@
+import CampaignList from '@/app/components/campaignAsset/CampaignList';
+import getCampaignListByMerchant from '@/app/components/merchant/campaigns/page';
+import { render, waitFor, screen } from "@testing-library/react";
+
+describe('Campaign List Component', () => {
+    it('renders Campaign List within container', async () => {
+        render(<CampaignList campaigns={[]} currentSessionUser={{ email: "", name: "", role: "" }} />)
+        await waitFor(() => {
+            const campaignListByMerchantElement = screen.getByTestId('campaign-list-by-merchant');
+            expect(campaignListByMerchantElement).toBeInTheDocument();
+        });
+    })
+
+    it('renders Campaign List has data', async () => {
+        render(<CampaignList campaigns={[]} currentSessionUser={{ email: "", name: "", role: "" }} />)
+        await waitFor(async () => {
+            const data = await getCampaignListByMerchant();
+            expect(data).toBeNull;
+        })
+    });
+
+});
