ae621fc2d0ee7be52c1e810377a0459a091d89f7
KhinYadanarNg <e0015793@u.nus.edu>
2024-03-24 16:17:48 +0800

    add required field values test case  in create campaign form

--- __tests__/CreateCampaignForm.test.tsx
+++ __tests__/CreateCampaignForm.test.tsx
@@ -1,5 +1,5 @@
 import CreateCampaignForm from '@/app/components/merchant/campaigns/createCampaign/CreateCampaignForm';
-import { Label } from '@headlessui/react/dist/components/label/label';
+import { StoreDetailProps } from '@/type/store';
 import { render, waitFor, screen, within } from "@testing-library/react";
 
 jest.mock('next/navigation');
@@ -13,4 +13,35 @@ describe('Create Campaign Form Component', () => {
             expect(campaignFormElement).toBeInTheDocument();
         });
     })
+
+
+    it('Create campaign form required field', () => {
+        const storeList: StoreDetailProps[] =  [{
+            storeId: '',
+            storeName: '',
+            description: '',
+            address: '',
+            address1: '',
+            address2: '',
+            address3: '',
+            city: '',
+            state: '',
+            country: '',
+            postalCode: '',
+            image: '',
+            contactNumber: ''
+        }]
+        render(<CreateCampaignForm stores={storeList} currentSessionUser={{ email: "", name: "", role: "" }} />)
+        if (storeList.length > 0) {
+            const titleTextField = screen.getByTestId("title-textfield-id");
+            expect(titleTextField).toBeRequired();
+    
+            const startDateTextField = screen.getByTestId("startdate-textfield-id");
+            expect(startDateTextField).toHaveAttribute('required');
+        } else {
+            const titleTextField = screen.getByTestId("empty-data");
+            expect(titleTextField).toBeInTheDocument
+        }
+      
+    });
 });
