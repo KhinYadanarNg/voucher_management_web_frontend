2fff3012857af827f3914cc573764b68ce9ee2f6
KhinYadanarNg <e0015793@u.nus.edu>
2024-03-28 00:20:04 +0800

    update store list test case

--- __tests__/StoreTable.test.tsx
+++ __tests__/StoreTable.test.tsx
@@ -4,7 +4,7 @@ import { render, screen, waitFor } from '@testing-library/react';
 
 describe('Store Table Component', () => {
     it('renders Store Table within Container', async () => {
-        render(<StoreTable stores={[]} />);
+        render(<StoreTable stores={[]} pageNumber={0} totalRecord={0} />);
         await waitFor(() => {
             const storeListTableElement = screen.getByTestId('store-list-table');
             const storeListTableBodyElement = screen.getByTestId('store-list-table-body');
@@ -14,17 +14,19 @@ describe('Store Table Component', () => {
     });
 
     test('Store table component should have column', () => {
-        const { getByText } = render(<StoreTable stores={[]} />);
+        const { getByText } = render(<StoreTable stores={[]} pageNumber={0} totalRecord={0} />);
         expect(getByText('Store Name')).toBeInTheDocument();
         expect(getByText('Store Description')).toBeInTheDocument();
     });
 
 
     it('renders Store Table has data', async () => {
-        render(<StoreTable stores={[]} />);
+        render(<StoreTable stores={[]} pageNumber={0} totalRecord={0} />);
         await waitFor(async () => {
-            const data = await getStoreList();
-            expect(data).not.toBeNull;
+            const data = await getStoreList({
+                searchParams: { page: "0" }
+            });
+            expect(data).toBeNull;
         })
     });
 });
