93d257afa159e977dcbc7a1621cee86d26c02c21
ThazinWai63 <139443552+WaiTz63@users.noreply.github.com>
2024-03-29 17:21:41 +0800

    differentiate api call cs call and ss call

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
 module.exports = () => {
   
-  let serverURL;
-  let serverAPIURL;
-
-  if (process.env.NODE_ENV === "production") {
-    serverURL = process.env.NEXT_PUBLIC_BACKEND_URL_PRD;
-    
-  } else if (process.env.NODE_ENV === "sit") {
-    serverURL = process.env.NEXT_PUBLIC_BACKEND_URL;
-    serverAPIURL = process.env.NEXT_PUBLIC_BACKEND_URL_INT;
-  } else {
-    serverURL = "http://localhost:8081"; 
-  }
 
   return {
-    env: {
-      serverURL,
-      serverAPIURL,
-    },
+  
     images: {
       remotePatterns: [
         {
