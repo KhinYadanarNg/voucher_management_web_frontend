88d007356bdcdf346c533348bd5634b3ad832101
ThazinWai63 <139443552+WaiTz63@users.noreply.github.com>
2024-03-31 17:35:54 +0800

    Added spinning for campaign list failed

--- app/components/customer/campaigns/page.tsx
+++ app/components/customer/campaigns/page.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { fetchCampaignsByCustomer} from '@/app/service/campaign';
-import { getCurrentUser } from '@/app/auth/getCurrentUser';
-import NullData from '../../common/NullData';
-import CampaignList from '../../campaigns/campaignAsset/CampaignList';
+import React from "react";
+import { fetchCampaignsByCustomer } from "@/app/service/campaign";
+import { getCurrentUser } from "@/app/auth/getCurrentUser";
+import NullData from "../../common/NullData";
+import CampaignList from "../../campaigns/campaignAsset/CampaignList";
+import { Spinner } from "@nextui-org/react";
 
 const getCampaignListByCustomer = async () => {
   try {
@@ -11,29 +12,33 @@ const getCampaignListByCustomer = async () => {
   } catch (error) {
     console.log(error);
   } finally {
-
   }
-}
+};
 
 export default async function CampaignListByCustomer() {
   var currentUser = await getCurrentUser();
   const campaigns = await getCampaignListByCustomer();
 
   if (!currentUser) {
-    currentUser = { email: "", name: "", image: "", role: "" }
+    currentUser = { email: "", name: "", image: "", role: "" };
   } else if (currentUser.role === "MERCHANT") {
     return <NullData title="Oops! Access denied" />;
   }
   {
     return (
       <div>
-        {campaigns ?
-          (<CampaignList campaigns={campaigns.data} currentSessionUser={currentUser} />
-          ) : (
-            <NullData title="Fetch data failed" />
-          )}
+        {campaigns ? (
+          <CampaignList
+            campaigns={campaigns.data}
+            currentSessionUser={currentUser}
+          />
+        ) : (
+          <div className="flex flex-col justify-center items-center h-screen">
+            <Spinner color="warning" size="lg" />
+            <span>Campaigns list is loading</span>
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
-
