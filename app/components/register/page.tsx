4b2e878beb1b85845030a1b661701d4b05f0949a
KhinYadanarNg <e0015793@u.nus.edu>
2024-04-07 17:10:54 +0800

    remove unnecessary import usage

--- app/components/register/page.tsx
+++ app/components/register/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Registration from './Registration'
 import Container from '../Container'
 import FormWrap from '../common/FormWrap'
 import RegisterForm from './RegisterForm'
