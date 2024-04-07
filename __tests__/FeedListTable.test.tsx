a78757fa2a4efaea366e31eb4f692eecb0655457
KhinYadanarNg <e0015793@u.nus.edu>
2024-04-07 17:11:38 +0800

    add feed list table test case

--- __tests__/FeedListTable.test.tsx
+++ __tests__/FeedListTable.test.tsx
@@ -1,18 +1,20 @@
-import FeedListCard from '@/app/components/customer/feeds/FeedListCard';
 import { render, waitFor, screen } from "@testing-library/react";
 import getAllActiveFeedListByCustomer from '@/app/components/customer/feeds/page';
+import FeedListTable from '@/app/components/customer/feeds/FeedListTable';
 
 describe('Feed List Component By Customer', () => {
     it('renders Feed List within container', async () => {
-        render(<FeedListCard feeds={[]} />)
+        render(<FeedListTable feeds={[]} />)
         await waitFor(() => {
-            const feedListElement = screen.getByTestId('feed-list-by-customer');
-            expect(feedListElement).toBeInTheDocument();
+            const feedListTable = screen.getByTestId('feed-list-table');
+            const feedListTableBody = screen.getByTestId('feed-list-table-body');
+            expect(feedListTable).toBeInTheDocument();
+            expect(feedListTableBody).toBeInTheDocument();
         });
     })
 
     it('renders Feed List has data', async () => {
-        render(<FeedListCard feeds={[]} />)
+        render(<FeedListTable feeds={[]} />)
         await waitFor(async () => {
             const data = await getAllActiveFeedListByCustomer();
             expect(data).not.toBeNull;
