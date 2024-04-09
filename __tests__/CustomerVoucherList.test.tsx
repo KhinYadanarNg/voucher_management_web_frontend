079d739c87ee6e7723348221ddfb2d07181433f2
KhinYadanarNg <e0015793@u.nus.edu>
2024-04-09 15:51:32 +0800

    update voucher list test case

--- __tests__/CustomerVoucherList.test.tsx
+++ __tests__/CustomerVoucherList.test.tsx
@@ -5,7 +5,7 @@ import { render, waitFor, screen } from "@testing-library/react";
 
 describe('Customer Voucher List Component', () => {
     it('renders Customer Voucher List within container', async () => {
-        render(<CustomerVouchersList vouchers={[]} currentSessionUser={{ email: "", name: "", role: "" }}/>)
+        render(<CustomerVouchersList vouchers={[]} currentSessionUser={{ email: "", name: "", role: "" }} pageNumber={0} totalRecord={0} size={0}/>)
         await waitFor(() => {
             const customerVoucherListElement = screen.getByTestId('voucher-list-by-customer');
             expect(customerVoucherListElement).toBeInTheDocument();
@@ -13,9 +13,11 @@ describe('Customer Voucher List Component', () => {
     })
 
     it('renders Customer Voucher List has data', async () => {
-        render(<CustomerVouchersList vouchers={[]} currentSessionUser={{ email: "", name: "", role: "" }}/>)
+        render(<CustomerVouchersList vouchers={[]} currentSessionUser={{ email: "", name: "", role: "" }} pageNumber={0} totalRecord={0} size={0}/>)
         await waitFor(async () => {
-            const data = await getVouchersByCustomerEmail()
+            const data = await getVouchersByCustomerEmail({
+                searchParams: { page: "0" }
+            })
             expect(data).not.toBeNull;
         })
     });
