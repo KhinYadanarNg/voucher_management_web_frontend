420f70b8c2563ecb82506c24f6d3271440206497
KhinYadanarNg <e0015793@u.nus.edu>
2024-04-15 22:46:32 +0800

    added pagination for feed list

--- __tests__/FeedListTable.test.tsx
+++ __tests__/FeedListTable.test.tsx
@@ -4,7 +4,7 @@ import FeedListTable from '@/app/components/customer/feeds/FeedListTable';
 
 describe('Feed List Component By Customer', () => {
     it('renders Feed List within container', async () => {
-        render(<FeedListTable feeds={[]} />)
+        render(<FeedListTable feeds={[]} pageNumber={0} totalRecord={0} size={0} />)
         await waitFor(() => {
             const feedListTable = screen.getByTestId('feed-list-table');
             const feedListTableBody = screen.getByTestId('feed-list-table-body');
@@ -14,9 +14,11 @@ describe('Feed List Component By Customer', () => {
     })
 
     it('renders Feed List has data', async () => {
-        render(<FeedListTable feeds={[]} />)
+        render(<FeedListTable feeds={[]} pageNumber={0} totalRecord={0} size={0} />)
         await waitFor(async () => {
-            const data = await getAllActiveFeedListByCustomer();
+            const data = await getAllActiveFeedListByCustomer({
+                searchParams: { page: "0" }
+            });
             expect(data).not.toBeNull;
         })
     });
