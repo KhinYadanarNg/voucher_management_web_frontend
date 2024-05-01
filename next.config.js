cb744a5e0721fd485f7a20227df248a96e64d1e7
KhinYadanarNg <e0015793@u.nus.edu>
2024-05-01 20:53:42 +0800

    update image host name for prod

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = () => {
       remotePatterns: [
         {
           protocol: "https",
-          hostname: "voucher-management.s3.ap-southeast-1.amazonaws.com",
+          hostname: "voucher-app-prd.s3.ap-southeast-1.amazonaws.com",
           hostname: "voucher-app-sit.s3.ap-southeast-1.amazonaws.com",
         },
       ],
